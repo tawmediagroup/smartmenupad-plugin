134c1974912c51e7cf79945668fe8363559b458a
Vipasha Katna <64221460+katnavipasha@users.noreply.github.com>
2022-07-25 19:48:54 +0530

    Remove DB table on uninstall

--- uninstall.php
+++ uninstall.php
@@ -3,6 +3,7 @@
 // Check
 defined('ABSPATH') || exit;
 defined('WP_UNINSTALL_PLUGIN') || exit;
+global $wpdb;
 
 // Remove options
 foreach (wp_load_alloptions() as $option => $value) {
@@ -10,3 +11,6 @@ foreach (wp_load_alloptions() as $option => $value) {
 		delete_option($option);
 	}
 }
+$table_name = $wpdb->prefix . "smp_quick_menu";
+$sql_delete = "DROP TABLE IF EXISTS $table_name";
+$wpdb->query( $sql_delete );
